c1f8e8d2a253b92a57c94dd36589f568b3b343b0
muhammadlee <yangbiao8169@foxmail.com>
2016-12-01 15:46:36 +0800

    Version0

---
+++ app/src/main/java/cn/edu/yangbiao8169nwsuaf/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package cn.edu.yangbiao8169nwsuaf.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO fuck yourself!
+        TextView textView = (TextView) findViewById(R.id.text1);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new buttonOnClickListener());
+
+    }
+
+    private static class buttonOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Log.i("he","hhe");
+        }
+    }
+}
---
+++ app/src/main/java/cn/edu/yangbiao8169nwsuaf/myapplication/box.java
@@ -0,0 +1,44 @@
+package cn.edu.yangbiao8169nwsuaf.myapplication;
+
+import android.graphics.drawable.shapes.RectShape;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/12/1.
+ */
+public class box extends RectShape {
+    public static final int INT = 10;
+//***************************
+//helllo
+//*************************
+
+
+    @Override
+    public boolean hasAlpha() {
+        return super.hasAlpha();
+    }
+
+    List<String> mList = new ArrayList<>();
+    int i=0;
+
+
+    public int getI() {
+        return  INT;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public List<String> getList() {
+        return mList;
+    }
+
+    public void setList(List<String> list) {
+        mList = list;
+    }
+
+}
